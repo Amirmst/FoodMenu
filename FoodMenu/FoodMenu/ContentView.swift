53dc12d73796c38e144c09f3ba86d86d4441daf0
Amir Mostafavi <amireza.mostafavi@yahoo.com>
2020-01-16 12:40:08 -0600

    initial commit

---
+++ FoodMenu/FoodMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodMenu
+//
+//  Created by Amir Mostafavi on 1/16/20.
+//  Copyright © 2020 amir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
